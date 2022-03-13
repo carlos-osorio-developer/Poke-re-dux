056b9322e394016ebd5584a184b45ccad00e0764
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-12 20:27:36 -0500

    Add header component using Semantic-UI-react

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Menu as SemanticMenu, Container, Image } from 'semantic-ui-react';
+import logo from '../assets/images/logo_v4.svg';
+
+const Header = () => (
+  <SemanticMenu fixed="top">
+    <Container>
+      <SemanticMenu.Item to="/">
+        <Image size="small" src={logo} />
+      </SemanticMenu.Item>
+      <SemanticMenu.Item to="/">Home</SemanticMenu.Item>
+      <SemanticMenu.Item to="/" position="right">
+        Favorites
+      </SemanticMenu.Item>
+    </Container>
+  </SemanticMenu>
+);
+
+export default Header;
