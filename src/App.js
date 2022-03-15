7d66547bf68ce3a2604ca13af7318da5b0fe6dac
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-15 14:44:44 -0500

    Add searchBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/header';
 import PokemonList from './components/pokemonList';
+import SearchBar from './components/searchBar';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <SearchBar />
       <PokemonList />
     </div>
   );
