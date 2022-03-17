d0d2fd6b3bde3f452293b6e25616f215ac29555d
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-16 19:20:06 -0500

    Set failure state for redux thunk

--- src/redux/slices/pokemonListSlice.js
+++ src/redux/slices/pokemonListSlice.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const SET_POKEMONS = 'pokeRedux/pokemonList/SET_POKEMONS';
 const GET_POKEMONS = 'pokeRedux/pokemonList/GET_POKEMONS';
 const GET_POKEMONS_SUCCESS = 'pokeRedux/pokemonList/GET_POKEMONS_SUCCESS';
+const GET_POKEMONS_FAILURE = 'pokeRedux/pokemonList/GET_POKEMONS_FAILURE';
 
 const URL = process.env.POKEAPI_URL || 'https://pokeapi.co/api/v2/';
 const axiosInstance = axios.create({
@@ -13,7 +14,11 @@ const getPokemons = (limit = 151) => async (dispatch) => {
   dispatch({ type: GET_POKEMONS });
   const request = await axiosInstance.get(`pokemon?limit=${limit}`);
   const data = await request.data.results;
-  dispatch({ type: GET_POKEMONS_SUCCESS, payload: data });
+  if (data) {
+    dispatch({ type: GET_POKEMONS_SUCCESS, payload: data });
+  } else {
+    dispatch({ type: GET_POKEMONS_FAILURE, payload: request.data });
+  }
 };
 
 const setPokemons = (payload) => ({
@@ -27,6 +32,8 @@ const pokemonListReducer = (state = [], action) => {
       return { ...state, loading: true };
     case GET_POKEMONS_SUCCESS:
       return { ...state, loading: false, pokemons: action.payload };
+    case GET_POKEMONS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
     case SET_POKEMONS:
       return { ...state, list: action.payload };
     default:
