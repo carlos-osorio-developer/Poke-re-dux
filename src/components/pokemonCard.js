93f4ec43ac2565b49ce8a48affe0994b93e0e154
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-16 19:59:15 -0500

    Render pokemons names using Redux State

--- src/components/pokemonCard.js
+++ src/components/pokemonCard.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Image, Label, Grid, Icon,
 } from 'semantic-ui-react';
 import style from './pokemonCard.module.css';
 
-const PokemonCard = () => (
-  <Grid.Column mobile={16} tablet={8} computer={4}>
-    <div className={style.PokemonCard}>
-      <button type="button" className={style.PokemonCardFavorite}>
-        <Icon name="heart" color="red" />
-      </button>
-      <Image
-        centered
-        src="https://static.wikia.nocookie.net/espokemon/images/0/03/Ditto.png"
-        alt="Pokemon img"
-      />
-      <p className={style.PokemonCardTitle}>Ditto</p>
-      <Label color="green"> Normal </Label>
-    </div>
-  </Grid.Column>
-);
+const PokemonCard = (props) => {
+  const { pokeName } = props;
+
+  return (
+    <Grid.Column mobile={16} tablet={8} computer={4}>
+      <div className={style.PokemonCard}>
+        <button type="button" className={style.PokemonCardFavorite}>
+          <Icon name="heart" color="red" />
+        </button>
+        <Image
+          centered
+          src="https://static.wikia.nocookie.net/espokemon/images/0/03/Ditto.png"
+          alt="Pokemon img"
+        />
+        <p className={style.PokemonCardTitle}>{pokeName}</p>
+        <Label color="green"> Normal </Label>
+      </div>
+    </Grid.Column>
+  );
+};
+
+PokemonCard.propTypes = {
+  pokeName: PropTypes.string.isRequired,
+};
 
 export default PokemonCard;
--- src/components/pokemonList.js
+++ src/components/pokemonList.js
@@ -1,24 +1,23 @@
 import { React, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PokemonCard from './pokemonCard';
 import { getPokemons } from '../redux/slices/pokemonListSlice';
 
 const PokemonList = () => {
   const dispatch = useDispatch();
-
-  const pokemons = Array(10).fill('');
   useEffect(() => {
     dispatch(getPokemons());
   }, []);
 
+  const pokemons = useSelector((state) => state.pokeState.pokemons) || [];
   let id = 0;
 
   return (
     <Grid className="PokemonList">
       {pokemons.map((pokemon) => {
         id += 1;
-        return <PokemonCard key={id} pokemon={pokemon} />;
+        return <PokemonCard key={id} pokeName={pokemon.name} />;
       })}
     </Grid>
   );
