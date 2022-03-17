8976bf998b118ffc370eac64d06861e80d334bf3
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-17 17:20:58 -0500

    Add styles to searchBar comp

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { FaSearch } from 'react-icons/fa';
 
 export default function SearchBar() {
   return (
-    <div className="Searcher wrapper">
-      <div className="Searcher__input">
-        <input type="text" placeholder="Search" />
-      </div>
-    </div>
+    <Form className="w-50 m-auto mt-5">
+      <Form.Group className="mb-3 pt-5 d-flex">
+        <Form.Control type="email" placeholder="Enter PokeName" />
+        <Button variant="primary" type="submit">
+          <FaSearch />
+        </Button>
+      </Form.Group>
+    </Form>
   );
 }
