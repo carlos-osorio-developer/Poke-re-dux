280bf8d20c49d52b065731c7814d3966bb111ad4
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-19 16:38:55 -0500

    Import favoriteSlice into the Store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import pokemonListReducer from './slices/pokemonListSlice';
+import favoritesReducer from './slices/favoritesSlice';
 
 const reducer = combineReducers({
   pokeState: pokemonListReducer,
+  favState: favoritesReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
--- src/redux/slices/favoritesSlice.js
+++ src/redux/slices/favoritesSlice.js
@@ -4,20 +4,29 @@ const DELETE_FAVORITE = 'pokeRedux/favorites/DELETE_FAVORITE';
 const saveFavorite = (payload) => ({
   type: SAVE_FAVORITE,
   payload,
-})
+});
 
 const deleteFavorite = (payload) => ({
   type: DELETE_FAVORITE,
   payload,
-})
+});
 
 const favoritesReducer = (state = [], action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SAVE_FAVORITE:
-      return { ...state, payload };
+      return { ...state, pokemons: action.payload };
     case DELETE_FAVORITE:
-      return { ...state, payload };
+      return {
+        ...state,
+        pokemons: state.pokemons.filter((pokemon) => (pokemon === action.payload)),
+      };
     default:
       return state;
   }
-}
+};
+
+export {
+  favoritesReducer as default,
+  saveFavorite,
+  deleteFavorite,
+};
